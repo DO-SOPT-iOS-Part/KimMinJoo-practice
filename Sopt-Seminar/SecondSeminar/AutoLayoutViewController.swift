3384d1f62b5a8a25d467252e0138198ec37454ee
frohsch <kminjoo1213@gmail.com>
2023-10-14 15:51:19 +0900

    [FEAT] #3 - Implement with Auto Layout

--- Sopt-Seminar/SecondSeminar/AutoLayoutViewController.swift
+++ Sopt-Seminar/SecondSeminar/AutoLayoutViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  AutoLayoutViewController.swift
 //  Sopt-Seminar
 //
 //  Created by Minjoo Kim on 10/14/23.
@@ -7,73 +7,71 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class AutoLayoutViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
-}
-
-final class FirstViewController_colorbox: UIViewController {
-    
-    private let colorbox = UIView().then {
-        $0.backgroundColor = .yellow
-    }
-    private let colorbox2 = UIView().then {
-        $0.backgroundColor = .green
-    }
-    
-    private let colorbox3 = UIView().then {
-        $0.backgroundColor = .black
-    }
-    private let colorbox4 = UIView().then {
-        $0.backgroundColor = .blue
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setLayout()
-    }
-    
-}
-private extension FirstViewController_colorbox {
-    
-    func style() {
-        
-        view.backgroundColor = .white
-    }
-    func setLayout() {
         self.view.backgroundColor = .white
-        self.view.addSubview(colorbox)
-        colorbox.snp.makeConstraints {
-            $0.top.leading.equalToSuperview()
-            $0.width.equalTo(UIScreen.main.bounds.width / 2)
-            $0.height.equalTo(UIScreen.main.bounds.height / 4)
+        self.setLayout()
+    }
+    
+    private let yellowColorbox: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
+    private let greenColorbox: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        return view
+    }()
+    
+    private let blackColorbox: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    private let blueColorbox: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        return view
+    }()
+}
+
+extension AutoLayoutViewController {
+    
+    private func setLayout() {
+        [yellowColorbox, greenColorbox, blackColorbox, blueColorbox].forEach { [weak self] view in
+            guard let self else {return}
+            view.translatesAutoresizingMaskIntoConstraints = false
+            self.view.addSubview(view)
         }
         
-        self.view.addSubview(colorbox2)
-        colorbox2.snp.makeConstraints {
-            $0.top.equalTo(colorbox.snp.bottom)
-            $0.leading.equalTo(colorbox.snp.trailing)
-            $0.width.equalTo(UIScreen.main.bounds.width / 2)
-            $0.height.equalTo(UIScreen.main.bounds.height / 4)
-        }
-        self.view.addSubview(colorbox3)
-        colorbox3.snp.makeConstraints {
-            $0.top.equalTo(colorbox2.snp.bottom)
-            $0.leading.equalToSuperview()
-            $0.width.equalTo(UIScreen.main.bounds.width / 2)
-            $0.height.equalTo(UIScreen.main.bounds.height / 4)
-        }
-        self.view.addSubview(colorbox4)
-        colorbox4.snp.makeConstraints {
-            $0.top.equalTo(colorbox3.snp.bottom)
-            $0.leading.equalTo(colorbox.snp.trailing)
-            $0.width.equalTo(UIScreen.main.bounds.width / 2)
-            $0.height.equalTo(UIScreen.main.bounds.height / 4)
-        }
+        NSLayoutConstraint.activate([yellowColorbox.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0),
+                                     yellowColorbox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+                                     yellowColorbox.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2),
+                                     yellowColorbox.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 4)])
+        
+        NSLayoutConstraint.activate([greenColorbox.topAnchor.constraint(equalTo: self.yellowColorbox.bottomAnchor, constant: 0),
+                                     greenColorbox.leadingAnchor.constraint(equalTo: self.yellowColorbox.trailingAnchor, constant: 0),
+                                     greenColorbox.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2),
+                                     greenColorbox.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 4)])
+        
+        NSLayoutConstraint.activate([blackColorbox.topAnchor.constraint(equalTo: self.greenColorbox.bottomAnchor, constant: 0),
+                                     blackColorbox.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0),
+                                     blackColorbox.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2),
+                                     blackColorbox.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 4)])
+        
+        NSLayoutConstraint.activate([blueColorbox.topAnchor.constraint(equalTo: self.blackColorbox.bottomAnchor, constant: 0),
+                                     blueColorbox.leadingAnchor.constraint(equalTo: self.blackColorbox.trailingAnchor, constant: 0),
+                                     blueColorbox.widthAnchor.constraint(equalToConstant: UIScreen.main.bounds.width / 2),
+                                     blueColorbox.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height / 4)])
+        
+
     }
 }
